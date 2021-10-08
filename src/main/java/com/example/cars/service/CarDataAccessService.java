2a154dc43bbee23ac7e35614f09d68df6e40c3a1
Jacob Doren <jakedoren@gmail.com>
2021-10-07 21:43:30 -0400

    carDataAccessService

--- src/main/java/com/example/cars/service/CarDataAccessService.java
+++ src/main/java/com/example/cars/service/CarDataAccessService.java
@@ -16,8 +16,11 @@ public class CarDataAccessService {
     }
 
     public Car insertCar(Car car) {
-        if(car.getMake().contains("2")) {
+        if(car.getMake().isBlank() || car.getModel().isBlank()) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Please enter in a Make and Model");
+        }
+        if(car.getPrice() == null || car.getPrice().isNaN()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Please enter a number for a price");
         }
         return repository.insert(car);
     }
